1a49977a0689ebaca9293425dc601d5c6304028d
Ionut Cristian Mintar <ionutcristian_mintar@yahoo.com>
2021-04-17 20:04:34 +0100

    Implemented hit points

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -4,18 +4,3 @@ feature 'Enter names' do
     expect(page).to have_content 'Hulk vs. Thor'
   end
 end
-
-feature "View hit points" do 
-  scenario 'see Player 2 hit points' do
-    sign_in_and_play
-    expect(page).to have_content 'Thor: 60HP'
-  end
-end
-
-feature 'Attacking' do
-  scenario 'attack Player 2' do
-    sign_in_and_play
-    click_button 'Attack'
-    expect(page).to have_content 'Hulk attacked Thor'
-  end
-end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,34 @@
 require 'player'
 
 describe Player do 
-  subject(:hulk) {Player.new("Hulk")} 
-
+  subject(:hulk) { Player.new("Hulk") } 
+  subject(:thor) { Player.new("Thor")}
+  
   describe '#name' do
     it 'returns the name' do
       expect(hulk.name).to eq "Hulk" 
     end
   end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(thor.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(thor).to receive(:receive_damage)
+      hulk.attack(thor)
+    end
+  end
+  
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { hulk.receive_damage }.to change { hulk.hit_points }.by(-10)
+    end
+  end
+
+
+
 end
