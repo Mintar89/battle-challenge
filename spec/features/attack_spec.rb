927ff2b9792de98f8db3ab754d279f607ce9c7a8
Ionut Cristian Mintar <ionutcristian_mintar@yahoo.com>
2021-04-19 12:04:01 +0100

    Enabled multiplayer

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,9 +4,15 @@ feature 'Attacking' do
     click_button 'Attack'
     expect(page).to have_content 'Hulk attacked Thor'
   end
-end
 
-feature 'Attacking' do 
+  scenario 'be attacked by Player 2' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'OK'
+    click_button 'Attack'
+    expect(page).to have_content 'Thor attacked Hulk'
+  end
+
   scenario "reduce Player 2 HP by 10" do
     sign_in_and_play
     click_button 'Attack'
@@ -14,4 +20,15 @@ feature 'Attacking' do
     expect(page).not_to have_content 'Thor: 60HP'
     expect(page).to have_content "Thor: 50HP"
   end
+  
+  scenario 'reduce Player 1 HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'OK'
+    click_button 'Attack'
+    click_button 'OK'
+    expect(page).not_to have_content 'Hulk: 60HP'
+    expect(page).to have_content 'Hulk: 50HP'
+  end
+
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,4 +1,9 @@
 feature "View hit points" do 
+  scenario 'see Player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Hulk: 60HP'
+  end
+  
   scenario 'see Player 2 hit points' do
     sign_in_and_play
     expect(page).to have_content 'Thor: 60HP'
